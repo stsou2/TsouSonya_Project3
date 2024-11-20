ca8523d4f2d60fb1a6629b9ab8e399b8e0b6881b
stsou2 <stsou2@uwo.ca>
2024-11-20 16:35:10 -0500

    Part 1 - attempting to fix function. Prints correct value for pc[0], but the for loop is exceedingly slow

--- TsouSonya_Project3.py
+++ TsouSonya_Project3.py
@@ -13,43 +13,51 @@ def dSstate_dr(r, S):
     Args:
 
     r (array-like): dependant variable times
-    S (array-like): state vector [p, m, dp/dr, dm/dr]
+    S (array-like): state vector [p, m]
    
     Returns:
 
-    dSstate (array): state vector [dp/dr, dm/dr, d2p/dr2, d2m/dr2] 
+    dSstate (array): state vector [dp/dr, dm/dr]
     '''
-    dSstate = [0,0,0,0] # ie [dp/dr, dm/dr, d2p/dr2, d2m/dr2]
+    dSstate = [0,0] # ie [dp/dr, dm/dr]
     p = S[0]
     m = S[1]
 
-    x = p**(1/3)
+    #x = p**(1/3)
+    x = np.sign(p) * (np.abs(p)) ** (1/3)
     gamma = (x**2)/(3*((1+x**2)**0.5))
  
     #derivatives 
-    dSstate[0] = S[2] 
-    dSstate[1] = S[3]
-    dSstate[2] = -(m*p)/(gamma*r**2)
-    dSstate[3] = (r**2)*p
+    dSstate[0] = -(m*p)/(gamma*r**2)
+    dSstate[1] = (r**2)*p
     
     return dSstate
 
 # This is the function that returns the thing we want to 'zero' - events in solve ivp makes this equal to 0
 def maxR(r, S):
-        return dSstate_dr(r,S)[0]
+        if S[0] < 1e-5:
+            return 0
+        else:
+             return S[0]
 
-pc = np.linspace(0.1, 2.5*10**6, 10, endpoint=True)
+pc = np.linspace(0.1, 2.5*10**6, 10)
+ue = 2
+R0 = (7.72*10**8/ue)
+M0 = 5.67*10**33/(ue**2)
+p0 = 9.74*10**5*ue*const.g0
 
 fig, ax = plt.subplots() 
 
 for i in range(len(pc)):
-    sol = sc.integrate.solve_ivp(dSstate_dr, (0.01, 30000), np.array([pc[0],0,0,0]), t_eval=np.linspace(0.01, 30000, 1000), events = maxR)
-    ax.plot(sol.t, sol.y[0], label = f'pc={pc[i]}')
+    sol = sc.integrate.solve_ivp(dSstate_dr, (0.01, 10), np.array([pc[i],0]), events = maxR)
+    ax.plot(sol.t_events[0][0]*R0/6.957e10, sol.y_events[0][0][1]*M0/2e33, 'o') #t is r and y[0] is p, y[1] is m
+    print(i, 'done')
 
-  # "y is an array of arrays, so we use y[0]"
-# ax.set_title('Solution of the undamped, undriven ODE')  # title and axis labels
-# ax.set_xlabel('Time (s)') 
-# ax.set_ylabel('theta(t)')  
+#ax.plot(sol.t*R0, sol.y[0]*p0, label = f'pc={pc[1]}') #t is r and y[0] is p, y[1] is m
+
+ax.set_title('r vs m')  # title and axis labels
+ax.set_xlabel('m') 
+ax.set_ylabel('r')  
 ax.legend()
 plt.grid(True)  # adding grid
 # plt.savefig('TsouSonya_Lab5_Fig1.png') # save to file
