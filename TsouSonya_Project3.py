7a05b2ab28e442af105e913b87aa345f03f7a316
stsou2 <stsou2@uwo.ca>
2024-11-23 16:44:01 -0500

    Fixed values; integration start was set too large. Fixed plot and printed estimated values comparaison

--- TsouSonya_Project3.py
+++ TsouSonya_Project3.py
@@ -23,8 +23,7 @@ def dSstate_dr(r, S):
     p = S[0]
     m = S[1]
 
-    #x = p**(1/3)
-    x = np.sign(p) * (np.abs(p)) ** (1/3)
+    x = np.sign(p) * (np.abs(p)) ** (1/3) # numpy throws warning for fractional powers of negative numbers
     gamma = (x**2)/(3*((1+x**2)**0.5))
  
     #derivatives 
@@ -34,15 +33,24 @@ def dSstate_dr(r, S):
     return dSstate
 
 # This is the function that returns the thing we want to 'zero' - events in solve ivp makes this equal to 0
+# In this case, the density will go to 0 at our desired max radius
 def maxR(r, S):
-        if S[0] < 1e-5:
-            return 0
-        else:
-             return S[0]
+    '''
+    Args:
 
-pc = np.logspace(-1, 6, 100)
-#pc = np.linspace(10e-1, 2.5*10e6, 10)
-#np.append(pc, 2.5*10e6)
+    r (array-like): dependant variable times
+    S (array-like): state vector [p, m]
+   
+    Returns:
+
+    S[0]: event for which we want to find the root of; returns S[0] = 0 for small enough values of S[0]
+    '''
+    if S[0] < 1e-5: # events detects a 0 via a sign change; since the density will not be negative, we set a close enough bound to prevent infinite iteration
+        return 0
+    else:
+        return S[0]
+
+pc = np.logspace(-1, 6.4, 10, endpoint=True)
 ue = 2
 R0 = (7.72*10**8/ue)
 M0 = 5.67*10**33/(ue**2)
@@ -51,18 +59,20 @@ p0 = 9.74*10**5*ue*const.g0
 fig, ax = plt.subplots() 
 
 for i in range(len(pc)):
-    maxR.terminal = True
-    sol = sc.integrate.solve_ivp(dSstate_dr, (0.01, 10), np.array([pc[i],0]), events = maxR)
-    print(sol.t_events[0][0])
-    print(sol.y_events[0][0][1])
-    ax.plot(sol.t_events[0][0]*R0/6.957e10, sol.y_events[0][0][1]*M0/2e33, marker = 'o') #t is r and y[0] is p, y[1] is m
-    print(i, 'done')
+    maxR.terminal = True # Will terminate once event has been reached. Value of interest will be the singular value in sol.t_events/y_events or the last value in sol.t/y
+    sol = sc.integrate.solve_ivp(dSstate_dr, (1e-8, 1e5), np.array([pc[i],0]), method = 'RK45', events = maxR)
+    radius = ((sol.t_events[0][0]*R0)*u.cm).to(u.solRad) # converting radius to solar radii
+    mass = ((sol.y_events[0][0][1]*M0)*u.g).to(u.solMass) # converting mass to solar mass
+    ax.plot(mass, radius, 'bo')
 
-#ax.plot(sol.t*R0, sol.y[0]*p0, label = f'pc={pc[1]}') #t is r and y[0] is p, y[1] is m
-
-ax.set_title('r vs m')  # title and axis labels
-ax.set_xlabel('m') 
-ax.set_ylabel('r')  
+ax.set_title('White Dwarf: Radius vs Mass')  # title and axis labels
+ax.set_xlabel('Mass (solar mass units)') 
+ax.set_ylabel('Radius (solar radii)')
+plt.axvline(mass.value, color = 'r') # set a vertical line at the last tested value; is close to radius = 0  
 plt.grid(True)  # adding grid
-# plt.savefig('TsouSonya_Lab5_Fig1.png') # save to file
+plt.savefig('TsouSonya_Project3_Fig1_RvMplot.png') # save to file
+
+print(f"Estimated Chandrasekhar limit: {mass}")
+print(f"Kippenhahn & Weigert (1990) MCh: {5.836/(ue**2)*u.solMass} ")
+print(f"Estimated mass limit is about {round(np.abs(((mass.value-5.836/(ue**2))/(5.836/(ue**2)))*100), 3)}% less")
 plt.show()
