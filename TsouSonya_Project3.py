9b8af9ba39837a03cad6e232669ce99b2d2cf4f8
stsou2 <stsou2@uwo.ca>
2024-11-24 16:01:08 -0500

    Finished. Added function to iterate solving for lists of pc values, and adapted rest of code to use that instead for efficiency. Also added final comments

--- TsouSonya_Project3.py
+++ TsouSonya_Project3.py
@@ -49,95 +49,108 @@ def maxR(r, S):
     else:
         return S[0]
 
+def solve_pc(pc, method = 'RK45', solar = True):
+    '''
+    Args:
+    pc (array-like): List of pc values to test
+    method (string): solve_ivp method to use, defaults to the solve_ivp default RK45
+    solar (boolean): Converts radius and mass to solar units if True, else leaves in cgs
+
+    Returns:
+    radius_arr (array): list of maximum radius for given pc values
+    mass_arr (array): list of maximum mass for given pc values
+    '''
+    radius_arr = np.zeros(len(pc))
+    mass_arr = np.zeros(len(pc))
+    maxR.terminal = True # Will terminate once event has been reached. Value of interest will be the singular value in sol.t_events/y_events or the last value in sol.t/y
+
+    for i in range(len(pc)):
+        sol = sc.integrate.solve_ivp(dSstate_dr, (1e-8, 1e5), np.array([pc[i],0]), method = method, events = maxR)
+        radius = sol.t_events[0][0]*R0*u.cm # converting dimensionless radius to cgs
+        mass = sol.y_events[0][0][1]*M0*u.g # converting dimensionless mass to cgs
+        if solar == True: # convert cgs to solar units
+            radius = radius.to(u.solRad)
+            mass = mass.to(u.solMass)
+
+        radius_arr[i] = radius.value # np array does not allow storage of dimensional quantities
+        mass_arr[i] = mass.value
+  
+    return radius_arr, mass_arr
+
+
+
 ########## Part 2
 
-pc = np.logspace(-1, np.log(2.5e6), 10, endpoint=True)
+# Declare constants
 ue = 2
 R0 = (7.72*10**8/ue)
 M0 = 5.67*10**33/(ue**2)
 p0 = 9.74*10**5*ue*const.g0
 
+# Testing for 10 values of pc
+pc = np.logspace(-1, np.log10(2.5e6), 10, endpoint=True)
+radius_arr, mass_arr = solve_pc(pc)
+
+# Plotting
 fig, ax = plt.subplots() 
-
-maxR.terminal = True # Will terminate once event has been reached. Value of interest will be the singular value in sol.t_events/y_events or the last value in sol.t/y
-for i in range(len(pc)):
-    sol = sc.integrate.solve_ivp(dSstate_dr, (1e-8, 1e5), np.array([pc[i],0]), method = 'RK45', events = maxR)
-    radius = ((sol.t_events[0][0]*R0)*u.cm).to(u.solRad) # converting radius to solar radii
-    mass = ((sol.y_events[0][0][1]*M0)*u.g).to(u.solMass) # converting mass to solar mass
-    ax.plot(mass, radius, 'bo')
-
+ax.scatter(mass_arr, radius_arr)
 ax.set_title('White Dwarf: Radius vs Mass')  # title and axis labels
 ax.set_xlabel('Mass (solar mass units)') 
-ax.set_ylabel('Radius (solar radii)')
-plt.axvline(mass.value, color = 'r') # set a vertical line at the last tested value; is close to radius = 0  
+ax.set_ylabel('Radius (solar radius)')
+plt.axvline(mass_arr[-1], color = 'r') # set a vertical line at the last tested value; is very close to radius = 0  
 plt.grid(True)  # adding grid
 plt.savefig('TsouSonya_Project3_Fig1_RvMplot.png') # save to file
 
-print(f"Estimated Chandrasekhar limit: {mass}")
+print(f"Estimated Chandrasekhar limit: {mass_arr[-1]*u.solMass}")
 print(f"Kippenhahn & Weigert (1990) MCh: {5.836/(ue**2)*u.solMass} ")
-print(f"Estimated mass limit is about {round(((mass.value-5.836/(ue**2))/(5.836/(ue**2)))*100, 3)}% more")
+print(f"Estimated mass limit is about {round(((mass_arr[-1]-5.836/(ue**2))/(5.836/(ue**2)))*100, 3)}% more")
 #plt.show()
+
 
 ########## Part 3
 
-import pandas as pd #for simpler table comparaison
+import pandas as pd # just for simpler table making with headers
 
-pc = np.logspace(-1, np.log(2.5e6), 3, endpoint=True)
-methods = ['RK45', 'BDF']
+# Testing default RK45 and different BDF solve_ivp methods for 3x pc values
+pc = np.logspace(-1, np.log10(2.5e6), 3, endpoint=True)
+radRK45, massRK45 = solve_pc(pc, method = 'RK45')
+radBDF, massBDF = solve_pc(pc, method = 'BDF')
 
-vals3 = np.zeros((len(pc), len(methods)*3))
-
-for i in range(len(pc)):
-    for method in methods:
-        sol = sc.integrate.solve_ivp(dSstate_dr, (1e-8, 1e5), np.array([pc[i],0]), method = method, events = maxR)
-        rad = (sol.t_events[0][0]*R0) 
-        mass = (sol.y_events[0][0][1]*M0)
-
-        vals3[i, methods.index(method)] = rad
-        vals3[i, methods.index(method)+3] = mass 
-
-vals3[:,2] = (np.abs(vals3[:,0]-vals3[:,1])/((vals3[:,0]+vals3[:,1])*0.5))*100 # 3rd col is percent diff between the radii calculated for each method
-vals3[:,5] = (np.abs(vals3[:,3]-vals3[:,4])/((vals3[:,3]+vals3[:,4])*0.5))*100 # 5th col is percent diff between the mass calculated for each method
-
-vals3[:,0:2] = (vals3[:,0:2]*u.cm).to(u.solRad) #converting units
-vals3[:,3:5] = (vals3[:,3:5]*u.g).to(u.solMass)
+# Finding percent differences using |A-B|/((A+B)/2) * 100 percent diff formula
+diff_rad = np.abs(radRK45-radBDF)/((radRK45+radBDF)*0.5) * 100
+diff_mass = np.abs(massRK45-massBDF)/((massRK45+massBDF)*0.5) * 100
 
 print("\nPart 3")
-print(pd.DataFrame(vals3, columns=['Radius: RK45', 'Radius: BDF', '% Diff', 'Mass: RK45', 'Mass: BDF', '% Diff'], index = pc))
+print(pd.DataFrame(data = [pc, radRK45, radBDF, diff_rad, massRK45, massBDF, diff_mass], index=['Pc Value', 'Radius: RK45', 'Radius: BDF', '% Diff', 'Mass: RK45', 'Mass: BDF', '% Diff']).T)
 # Results are less than or about 1% different at most
 
 
 
 ########## Part 4
 
+# Loading data. I have the csv file stored in my github repo folder within my open folder hence the load name.
 data = np.loadtxt('TsouSonya_Project3/wd_mass_radius.csv', delimiter = ',', skiprows=1) 
 M_Msun = data[:, 0]  
 M_unc = data[:, 1]  
 R_Rsun = data[:, 2]
 R_unc = data[:, 3]
 
-# Making interpolation function. This will allow checking if the computed ODE is within range of an observed radius/mass with error    
-radius_list = []
-mass_list=[]
-pc = np.logspace(-1, np.log(2.5e6), 1000, endpoint=True)
+# Making interpolation function to plot computed ODE solution with
+pc = np.logspace(-1, np.log10(2.5e6), 1000, endpoint=True)
+radius_arr, mass_arr = solve_pc(pc)
+f = sc.interpolate.interp1d(mass_arr, radius_arr)
 
-for i in range(len(pc)):
-    sol = sc.integrate.solve_ivp(dSstate_dr, (1e-8, 1e5), np.array([pc[i],0]), method = 'RK45', events = maxR)
-    radius = ((sol.t_events[0][0]*R0)*u.cm).to(u.solRad) # converting radius to solar radii
-    mass = ((sol.y_events[0][0][1]*M0)*u.g).to(u.solMass) # converting mass to solar mass
-    radius_list.append(radius.value)
-    mass_list.append(mass.value)
-
-f = sc.interpolate.interp1d(mass_list, radius_list)
-
+# Plotting
 fig, ax2 = plt.subplots()
-ax2.errorbar(x=M_Msun, y=R_Rsun, yerr = R_unc, xerr = M_unc, fmt='o', label = 'Observed')
-ax2.plot(mass_list, f(mass_list), '-', label = 'Computed')
+ax2.errorbar(x=M_Msun, y=R_Rsun, yerr = R_unc, xerr = M_unc, fmt='o', label = 'Observed') # plotting given data with error
+ax2.plot(mass_arr, f(mass_arr), '-', label = 'Computed') # plotting computed solution
 ax2.set_xlabel('Mass (solar mass units)')
-ax2.set_ylabel('Radius (solar radii)')
+ax2.set_ylabel('Radius (solar radius)')
 ax2.set_title('White Dwarf: Radius vs Mass, Observed and Computed')
 plt.legend()
 plt.grid(True)
 plt.savefig('TsouSonya_Project3_Fig2_Obsvplot.png') # save to file
 
 plt.show()
+
+# Plot matches terribly with observational data, but that does appear to be normal for astronomy
